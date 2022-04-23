cb7a3f04150c1e8a5c77d3f057f976c1037ff43e
SalehMohammed01 <94100081+SalehMohammed01@users.noreply.github.com>
2022-04-23 18:49:48 +0530

    added all dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useQuery } from "react-query";
 
-function App() {
+// Components
+import { Drawer } from "@material-ui/core";
+import { LinearProgress } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import { Badge } from "@material-ui/core";
+
+// Styles
+import { Wrapper } from "./App.styles";
+
+// Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProduct = async (): Promise<CartItemType> => {
+  return await (await fetch('https://fakestoreapi.com/products')).json();
+}
+
+const App = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
     </div>
   );
 }
